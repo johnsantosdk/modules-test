efc17f34d2b868e6e59081a5af0ec372681b3296
Airton Santos <airton.santos3.1415@gmail.com>
2020-07-19 13:17:52 -0300

    fixed bug about import with babel

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import User, {printAge, printName} from './classes/user.js'
 
- const user = new User('John Master Dark', '27')
+ const user = new User('John Master Dark Matter', '27')
 printAge(user)
 printName(user)
 console.log(user)
